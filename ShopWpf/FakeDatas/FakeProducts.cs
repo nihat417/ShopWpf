1d45bb67c88d0d6c39bdbdfc6ab53cd817bb00b1
Nihat Akremi <nihatakremi@gmail.com>
2022-11-06 21:12:43 +0300

    Добавьте файлы проекта.

---
+++ ShopWpf/FakeDatas/FakeProducts.cs
@@ -0,0 +1,18 @@
+﻿using ShopProgramWpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProgramWpf.FakeDatas;
+
+public class FakeProducts
+{
+    public List<Product> products = new()
+    {
+        new Product("Iphone",12,12.5f),
+        
+    };
+}
---
+++ ShopWpf/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProgramWpf.Models;
+
+public class Product
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Name { get; set; }
+    public int? Count { get; set; }
+    public float? Cost { get; set; }
+
+    public Product(string Name, int Count, float Cost)
+    {
+        this.Name = Name;
+        this.Count = Count;
+        this.Cost = Cost;
+    }
+}
